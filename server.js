3c2bf1864942a78c68a081b4a4bba3f23743de6c
BHPriyanka <BHPriyanka@ccs.neu.edu>
2016-03-11 23:32:45 -0500

    Project POC- Basic layout of the pages Use of API to fetch results Implementation os Profile,Login,Registration pages

--- server.js
+++ server.js
@@ -1,25 +1,10 @@
 var express       = require('express');
 var app           = express();
-var bodyParser    = require('body-parser');
-var multer        = require('multer');
-var cookieParser  = require('cookie-parser');
-var session       = require('express-session');
-var upload = multer(); // for parsing multipart/form-data
-
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-//app.use(multer());
 
 app.use(express.static(__dirname + '/public'));
-var ipaddress = process. env .OPENSHIFT_NODEJS_IP || '127.0.0.1';
-var port = process. env .OPENSHIFT_NODEJS_PORT || 3000 ;
-/*app .get( 'https://news.google.com/news?&callback=?' , function (req, res){
-    console.log(req);
-    res.send(req);
-});*/
-/*var allowCrossDomain = function(req, res, next) {
-    res.header('Access-Control-Allow-Origin', "*");
-};*/
+var ipaddress = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
+var port = process.env.OPENSHIFT_NODEJS_PORT || 3000 ;
+
 require("./public/Experiments/omdbproject/server/app.js")(app);
 
-app . listen ( port , ipaddress );
+app.listen(port, ipaddress);
