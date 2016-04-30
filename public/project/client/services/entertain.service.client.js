11d271ff6c8eb6daeb93f330c2c615395ee21f13
BHPriyanka <BHPriyanka@ccs.neu.edu>
2016-04-30 01:49:05 -0400

    Changed view of profile,login, and set search results empy initially

--- public/project/client/services/entertain.service.client.js
+++ public/project/client/services/entertain.service.client.js
@@ -10,7 +10,7 @@
         return api;
 
         function findEntertainmentNews(title, page, callback){
-            var URL = "http://content.guardianapis.com/search?page=PAGE&format=json&tag=culture/culture,fashion/fashion&use-date=published&show-tags=contributor&show-fields=all&show-refinements=all&order-by=newest&api-key=617dcc89-35bf-4ae2-bf4d-c4c968ceb7a0&callback=JSON_CALLBACK&q=";
+            var URL = "http://content.guardianapis.com/search?page=PAGE&format=json&tag=culture/culture,fashion/fashion&use-date=published&show-tags=contributor&show-fields=all&show-refinements=all&order-by=newest&api-key=617dcc89-35bf-4ae2-bf4d-c4c968ceb7a0&callback=JSON_CALLBACK&page-size=15&q=";
             var url = URL.replace("PAGE", page);
             $http({
                 method: 'JSONP',
