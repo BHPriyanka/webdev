71ced6f3f9037f6feb39646fe5b57ee87ef6f7e7
BHPriyanka <BHPriyanka@ccs.neu.edu>
2016-04-30 01:55:50 -0400

    Changed view of profile,login, and set search results empy initially

--- public/project/client/services/techapi.service.client.js
+++ public/project/client/services/techapi.service.client.js
@@ -10,7 +10,7 @@
         return api;
 
         function findTechNews(title, page, callback){
-            var URL=  "http://content.guardianapis.com/search?format=json&tag=technology/technology,science/science&use-date=published&show-tags=contributor&show-fields=all&show-refinements=all&order-by=newest&api-key=617dcc89-35bf-4ae2-bf4d-c4c968ceb7a0&callback=JSON_CALLBACK&page=PAGE&q=";
+            var URL=  "http://content.guardianapis.com/search?format=json&tag=technology/technology,science/science&use-date=published&show-tags=contributor&show-fields=all&show-refinements=all&order-by=newest&api-key=617dcc89-35bf-4ae2-bf4d-c4c968ceb7a0&callback=JSON_CALLBACK&&page-size=15&page=PAGE&q=";
             var url = URL.replace("PAGE", page);
 
             $http({
